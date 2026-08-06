d1eb1bd1760770c9b35339611076e2f025928354
Devansh <devkulsh1308@gmail.com>
2026-08-06 20:57:31 +0530

    updated the phone numbers of kanpur branch in contact page

--- data/offices.ts
+++ data/offices.ts
@@ -7,7 +7,7 @@ export const offices: OfficeBranch[] = [
     image: "https://res.cloudinary.com/dhlqc0ymy/image/upload/v1783162422/kanpur_fzmknv.png",
     address:
       "117 N 65, Rani Ganj, Kakadeo, Kanpur, Uttar Pradesh 208025",
-    contacts: ["+91 8127942568", "+91 8423689480"],
+    contacts: ["+91 7521050567", "+91 8429133397", "+91 7275179977"],
     mapUrl:
       "https://www.google.com/maps/search/?api=1&query=117%20N%2065%20Rani%20Ganj%20Ambedkar%20Nagar%20Navin%20Nagar%20Kakadeo%20Kanpur%20Uttar%20Pradesh%20208025",
     status: "open",
